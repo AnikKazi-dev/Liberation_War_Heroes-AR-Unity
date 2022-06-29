90387c8fedf96b1593d00d79eb1f40ad0d3e072c
Kazi Anik Islam <anikkazi14@gmail.com>
2022-06-29 23:31:30 +0600

    1st commit

---
+++ Liberation War Heroes/Assets/Script/TrackableEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class TrackableEventHandler : DefaultTrackableEventHandler
+{
+    public UnityEvent onTrakingFoundEvent;
+    public UnityEvent onTrakingLostEvent;
+    public GameObject UIText;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        UIText.SetActive(false);
+        onTrakingFoundEvent.Invoke();
+    }
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        UIText.SetActive(true);
+        onTrakingLostEvent.Invoke();
+    }
+
+
+}
